2fa412ca2e80fe5cf76ba3d869cce407078fe90e
lichunbo123 <1164630272@qq.com>
2018-12-27 20:18:52 +0800

    add index

---
+++ js/detail.js
@@ -0,0 +1,13 @@
+// window.onload = function(){
+// 	var cover = document.getElementById('first');
+// 	window.onscroll = function(){
+// 		var st = document.documentElement.scrollTop || document.body.scrollTop;
+// 		if(st > 135){
+// 			cover.style.position = 'fixed';
+// 			var ii = document.getElementById("two");
+// 			ii.marginTop = 135 + "px";
+// 		}else{
+// 			cover.style.position = 'static';
+// 		}
+// 	}
+// }
